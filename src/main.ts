8aceb736d9713c1b876c827afb03a44311aeb253
carloncho <qarloztapia@gmail.com>
2024-07-20 17:02:12 -0400

    env

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: [process.env.CORS_URL],
     credentials: true,
   });
   await app.listen(process.env.PORT || 5000);
