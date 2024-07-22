49a738e4d4095ceebff95dc77a2451c37fdf9c74
carloncho <qarloztapia@gmail.com>
2024-07-21 22:42:10 -0400

    cors update

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
   app.enableCors({
-    origin: [process.env.CORS_URL],
+    origin: process.env.CORS_URL || 'https://chipper-meerkat-ae83d8.netlify.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization, Apollo-Require-Preflight',
   });
+  
+  app.useGlobalPipes(new ValidationPipe());
+  
   await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
